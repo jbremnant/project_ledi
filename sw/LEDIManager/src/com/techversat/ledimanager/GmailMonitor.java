f9dd656069b6dfca88ac51f49c13597a7221ecac
jbkim <jbremnant@gmail.com>
2012-12-21 18:27:33 -0500

    ledi android app

---
+++ sw/LEDIManager/src/com/techversat/ledimanager/GmailMonitor.java
@@ -0,0 +1,235 @@
+                                                                     
+package com.techversat.ledimanager;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.techversat.ledimanager.LEDIService.Preferences;
+
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+public class GmailMonitor {
+
+	Context context;
+	
+	MyContentObserver contentObserver = new MyContentObserver();
+	ContentResolver contentResolver;
+	
+	public static String account = "";
+	public static int lastUnreadGmailCount = 0;
+	
+	public GmailMonitor(Context ctx) {
+		super();		
+		context = ctx;
+	}
+
+	public void startMonitor() {
+		try {
+			AccountManager mgr = AccountManager.get(context);
+			Account[] accts = mgr.getAccounts();
+			final int count = accts.length;
+			Account acct = null;
+
+			for (int i = 0; i < count; i++) {
+				acct = accts[i];
+				if (acct.type.equals("com.google")) {
+					account = acct.name;
+					break;
+				}
+				// Log.d("ow",
+				// "eclair account - name="+acct.name+", type="+acct.type);
+			}
+
+			Uri uri = Uri.parse("content://gmail-ls/conversations/" + account);
+			contentResolver = context.getContentResolver();
+			contentResolver.registerContentObserver(uri, true, contentObserver);
+		} catch (Exception x) {
+			Log.d(LEDIActivity.TAG, x.toString());
+		}
+	}
+
+
+	private class MyContentObserver extends ContentObserver {
+
+		public MyContentObserver() {
+			super(null);
+		}
+
+		@Override
+		public void onChange(boolean selfChange) {
+			super.onChange(selfChange);
+
+			Log.d("ow", "onChange observer - unread");
+
+			if (Preferences.notifyGmail) 
+			{
+				int currentGmailUnreadCount = getUnreadGmailCount(account, "^u");
+
+				// Log.d("ow", "current gmail unread count: " + Integer.toString(currentGmailUnreadCount));
+
+				if (currentGmailUnreadCount > lastUnreadGmailCount)
+				{
+						Log.d("ow", Integer.toString(currentGmailUnreadCount) + " > " + Integer.toString(lastUnreadGmailCount));
+						sendUnreadGmail(account);						
+				}
+				
+				lastUnreadGmailCount = currentGmailUnreadCount;
+			}
+		}
+	}
+	
+	public int getUnreadGmailCount(String account, String label) {
+		// label = "^u";
+
+		try {
+			int nameColumn = 0;
+
+			Cursor c = context.getContentResolver().query(Uri.parse("content://gmail-ls/labels/" + account), null, null, null, null);
+
+			c.moveToFirst();
+
+			for (int i = 0; i < c.getColumnCount(); i++)
+				if (c.getColumnName(i).equals("canonicalName")) {
+					nameColumn = i;
+					break;
+				}
+
+			while (true) {
+				if (c.getString(nameColumn).equals(label))
+					// if (c.getString(nameColumn).equals("^i"))
+					for (int i = 0; i < c.getColumnCount(); i++) {
+						if (c.getColumnName(i).equals("numUnreadConversations")) {
+							return Integer.parseInt(c.getString(i));
+						}
+					}
+
+				c.moveToNext();
+
+				if (c.isLast()) {
+					break;
+				}
+			}
+		} catch (Exception x) {
+		}
+
+		return 0;
+	}
+	
+	
+	void sendUnreadGmail(String account) {
+		try {
+
+			int nameColumn = 0;
+			String id = "";
+			String convId = "";
+
+			double maxDate = 0;
+
+			Cursor c = context.getContentResolver().query(Uri.parse("content://gmail-ls/labels/" + account), null, null, null, null);
+			c.moveToFirst();
+
+			for (int i = 0; i < c.getColumnCount(); i++)
+				if (c.getColumnName(i).equals("canonicalName"))
+					nameColumn = i;
+
+			while (true) {
+				if (c.getString(nameColumn).equals("^u"))
+					for (int i = 0; i < c.getColumnCount(); i++) {
+						if (c.getColumnName(i).equals("_id")) {
+							id = c.getString(i);
+						}
+					}
+
+				if (c.isLast())
+					break;
+
+				c.moveToNext();
+			}
+
+			Cursor c2 = context.getContentResolver().query(Uri.parse("content://gmail-ls/conversations/" + account), null, null, null, null);
+			c2.moveToLast();
+
+			for (int i = 0; i < c2.getColumnCount(); i++)
+				if (c2.getColumnName(i).equals("labelIds"))
+					nameColumn = i;
+
+			while (true) {
+				if (c2.getString(nameColumn).indexOf(id) >= 0)
+					for (int i = 0; i < c2.getColumnCount(); i++) {
+						if (c2.getColumnName(i).equals("conversation_id"))
+							convId = c2.getString(i);
+					}
+
+				if (c2.isFirst())
+					break;
+
+				c2.moveToPrevious();
+			}
+			// ///////////////
+
+			maxDate = 0;
+
+			int colConvId = 0;
+			int colSub = 0;
+			int colFrom = 0;
+			int colRcv = 0;
+
+			String subject = "";
+			String sender = "";
+			String snippet = "";
+
+			Cursor c3 = context.getContentResolver().query(Uri.parse("content://gmail-ls/conversations/" + account + "/" + convId + "/messages"), null, null, null, null);
+			// startManagingCursor(c3);
+			c3.moveToFirst();
+
+			for (int i = 0; i < c3.getColumnCount(); i++) {
+				if (c3.getColumnName(i).equals("conversation"))
+					colConvId = i;
+				if (c3.getColumnName(i).equals("subject"))
+					colSub = i;
+				if (c3.getColumnName(i).equals("fromAddress"))
+					colFrom = i;
+				if (c3.getColumnName(i).equals("dateReceivedMs"))
+					colRcv = i;
+			}
+
+			while (true) {
+				if (c3.getString(colConvId).indexOf(convId) >= 0) {
+					double thisDate = Double.parseDouble(c3.getString(colRcv));
+
+					if (thisDate > maxDate) {
+						subject = c3.getString(colSub);
+						sender = c3.getString(colFrom);
+						snippet = c3.getString(c3.getColumnIndex("snippet"));
+						maxDate = thisDate;
+					}
+				}
+
+				if (c3.isLast())
+					break;
+
+				c3.moveToNext();
+			}
+
+			Pattern pattern = Pattern.compile("(\"[^\"]*\") (<.*>)");
+			Matcher matcher = pattern.matcher(sender);
+			matcher.find();
+
+	        String senderName = matcher.group(1).replace("\"", "");
+	        String senderMail = matcher.group(2).replace("<", "").replace(">", "");			
+			
+			NotificationBuilder.createGmail(context, senderName, senderMail, subject, snippet);
+
+		} catch (Exception x) {
+		}
+	}
+	
+}
---
+++ sw/LEDIManager/src/com/techversat/ledimanager/LEDIActivity.java
@@ -0,0 +1,167 @@
+package com.techversat.ledimanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.util.Log;
+
+// import org.techversat.ledimanager.R;
+import com.techversat.ledimanager.LEDIService;
+import com.techversat.ledimanager.Protocol;
+import com.techversat.ledimanager.Settings;
+// import org.techversat.ledimanager.Test;
+import com.techversat.ledimanager.Utils;
+import com.techversat.ledimanager.DeviceSelection;
+// import com.techversat.ledimanager.LEDIService.Preferences;
+import com.techversat.ledimanager.LEDIService.ConnectionState;
+
+
+public class LEDIActivity extends Activity {
+
+	public static final String TAG = "LEDI";
+	Context context;
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		context = this;
+		setContentView(R.layout.activity_main);
+		setTitle(getString(R.string.app_name));
+		Log.i(TAG, "onCreate");
+	}
+
+	public void startSearchActivity(View view) {
+	    Intent intent = new Intent(this, DeviceSelection.class);
+	    startActivity(intent);
+	}
+	
+	public void startLEDIService(View view) {
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		tv.setText("running LEDI Service");
+		startService();
+
+	}
+	
+	public void stopLEDIService(View view) {
+		stopService();
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		tv.setText("stopped LEDI Service");
+	}
+	
+	public void setLEDITime(View view)
+	{
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		if(LEDIService.connectionState != ConnectionState.CONNECTED)
+		{
+			tv.setText("Cannot set time. Please establish connection first");
+			return;
+		}
+		tv.setText("setting Time");
+		Protocol.sendRtcNow(this);
+	}
+	
+	public void sendLEDIText(View view)
+	{
+		EditText editText = (EditText) findViewById(R.id.editText1);
+		String message = editText.getText().toString();
+		Protocol.sendText(this, message);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		LEDIService.loadPreferences(this);
+		
+		/*
+		if (Preferences.idleMusicControls)
+			Protocol.enableMediaButtons();
+		//else 
+			//Protocol.disableMediaButtons();
+		
+		if (Preferences.idleReplay)
+			Protocol.enableReplayButton();
+		//else
+			//Protocol.disableReplayButton();
+		*/
+		
+		// Protocol.configureMode();
+		Log.i(TAG, "onStart");
+	}
+
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+	    case R.id.start:
+	    	startService();
+	        return true;
+	    case R.id.stop:	   
+	    	stopService();
+	        return true;
+	    case R.id.test:
+	    	// startActivity(new Intent(this, Test.class));
+	        return true;
+	    case R.id.settings:	 
+	    	startActivity(new Intent(this, Settings.class));
+	        return true;  
+	    case R.id.about:
+	    	showAbout();
+	        return true;
+	    case R.id.exit:	        
+	    	exit();
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+    
+	void startService() {
+		startService(new Intent(this, LEDIService.class));
+	}
+	
+    void stopService() {
+    	stopService(new Intent(this, LEDIService.class));
+    }
+    
+    void exit() {
+    	System.exit(0);
+    }
+    
+    void showAbout() {
+    	
+    	WebView webView = new WebView(this);
+		String html = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title>About</title></head><body>" + 
+						"<h1>LEDI</h1>" +
+						"<p>Version " + Utils.getVersion(this) + ".</p>" +
+						"<p>&copy; Copyright 2012 TechVersat LLC.</p>" +
+						"</body></html>";
+        webView.loadData(html, "text/html", "utf-8");
+        
+        new AlertDialog.Builder(this).setView(webView).setCancelable(true).setPositiveButton("OK", new DialogInterface.OnClickListener() {			
+			//@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		}).show();        
+    }
+}
